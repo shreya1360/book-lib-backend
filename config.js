69fb7c523690420fcf82bcdbe7eca08afbd638eb
Shreya Mishra <126142868+shreya1360@users.noreply.github.com>
2025-06-04 15:57:49 +0530

    fixed port

--- config.js
+++ config.js
@@ -8,9 +8,11 @@ const database = process.env.DATABASE_NAME;
 const user = process.env.DATABASE_USER;
 const password = process.env.DATABASE_PASSWORD;
 const host = process.env.DATABASE_HOST;
+const port = process.env.DATABASE_PORT;
 
 const sequelize = new Sequelize(database,user, password, {
   host: host,
+  port: port,
   dialect: "mysql",
 });
 
